398b2d51adc98412734ec6de68b7ce7cc9c1f0a2
happy-bean <1358876203@qq.com>
2018-09-11 10:45:44 +0800

    empty:创建一个空的Optional对象

---
+++ src/org/happybean/optional/OptionalEmpty.java
@@ -0,0 +1,24 @@
+package org.happybean.optional;
+
+import java.util.Optional;
+
+/**
+ * @author wgt
+ * @date 2018-09-11
+ * @description empty:创建一个空的Optional对象
+ **/
+public class OptionalEmpty {
+
+    public static void main(String[] args) {
+
+        Optional<String> optionalEmpty = createEmptyOptional();
+        System.out.println("optionalEmpty:" + optionalEmpty);
+    }
+
+    public static Optional createEmptyOptional() {
+
+        Optional optional = Optional.empty();
+        return optional;
+    }
+
+}
