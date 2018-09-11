cfcd3f160e56bf43ad47939bf18a4c728e9f3ecc
happy-bean <1358876203@qq.com>
2018-09-11 20:40:01 +0800

    list计数

---
+++ src/org/happybean/list/ListCount.java
@@ -0,0 +1,26 @@
+package org.happybean.list;
+
+import org.happybean.common.Person;
+
+import java.util.List;
+
+/**
+ * @author wgt
+ * @date 2018-09-11
+ * @description list计数
+ **/
+public class ListCount {
+
+    public static void main(String[] args) {
+
+        List<Person> list = Person.getPersonListData();
+        long count = personCountFilterByAge(list);
+        System.out.println("person count:" + count);
+    }
+
+    public static long personCountFilterByAge(List<Person> list) {
+
+        long count = list.stream().filter(person -> (person.getAge() > 19)).count();
+        return count;
+    }
+}
