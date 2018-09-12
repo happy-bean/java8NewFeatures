fbc9d1427eed0c6f3a76e08d82724e438d59c43a
happy-bean <1358876203@qq.com>
2018-09-12 10:19:09 +0800

    Stream 组合使用

---
+++ src/org/happybean/stream/StreamMix.java
@@ -0,0 +1,28 @@
+package org.happybean.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author wgt
+ * @date 2018-09-12
+ * @description 组合使用
+ **/
+public class StreamMix {
+
+    public static void main(String[] args) {
+
+        Integer[] nums = {1, 1, null, 2, 3, 4, null, 5, 6, 7, 8, 9, 10};
+        List<Integer> numList = Arrays.asList(nums);
+        System.out.println("sum:" + mix(numList));
+    }
+
+    public static int mix(List<Integer> list) {
+
+        int sum = list.stream().filter(num -> num != null).
+                distinct().mapToInt(num -> num * 2).
+                peek(System.out::println).skip(2).limit(4).sum();
+        return sum;
+
+    }
+}
