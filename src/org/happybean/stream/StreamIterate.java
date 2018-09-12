88d9fcc768cd8692b08a8f0172f46b0147f5ba67
happy-bean <1358876203@qq.com>
2018-09-12 10:05:01 +0800

    peek: 生成一个包含原Stream的所有元素的新Stream，同时会提供一个消费函数（Consumer实例），新Stream每个元素被消费的时候都会执行给定的消费函数

--- src/org/happybean/stream/StreamIterate.java
+++ src/org/happybean/stream/StreamIterate.java
@@ -7,6 +7,10 @@ import java.util.stream.Stream;
  * @date 2018-09-12
  * @description iterate方法：也是生成无限长度的Stream，和generator不同的是，其元素的生成是重复对给定的种子值(seed)调用用户指定函数来生成的。
  * 其中包含的元素可以认为是：seed，f(seed),f(f(seed))无限循环
+ * <p>
+ * 每条语句其实都是生成一个无限长度的Stream，其中值是随机的。
+ * 这个无限长度Stream是懒加载，一般这种无限长度的Stream都会配合Stream的limit()方法来用。
+ * </p>
  **/
 public class StreamIterate {
 
---
+++ src/org/happybean/stream/StreamPeek.java
@@ -0,0 +1,30 @@
+package org.happybean.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author wgt
+ * @date 2018-09-12
+ * @description peek: 生成一个包含原Stream的所有元素的新Stream，同时会提供一个消费函数（Consumer实例），
+ * 新Stream每个元素被消费的时候都会执行给定的消费函数
+ **/
+public class StreamPeek {
+
+    public static void main(String[] args) {
+
+        List<String> list = peek();
+        list.stream().forEach(System.out::println);
+    }
+
+    public static List<String> peek() {
+        List<String> list = Stream.of("a", "b", "c", "d")
+                .peek(e -> System.out.println("Peeked value: " + e))
+                .map(String::toUpperCase)
+                .peek(e -> System.out.println("Mapped value: " + e))
+                .collect(Collectors.toList());
+
+        return list;
+    }
+}
