f98dab0d4e67a11fdf032f3158c9d6faa60a1641
bean <18222997200@163.com>
2018-09-13 10:50:40 +0800

    Timezones:在新API中时区使用ZoneId来表示

---
+++ src/org/happybean/date/TimezonesDemo.java
@@ -0,0 +1,28 @@
+package org.happybean.date;
+
+import java.time.ZoneId;
+
+/**
+ * @author wgt
+ * @date 2018-09-13
+ * @description Timezones:在新API中时区使用ZoneId来表示。时区可以很方便的使用静态方法of来获取到。
+ * 时区定义了到UTS时间的时间差，在Instant时间点对象到本地日期对象之间转换的时候是极其重要的
+ **/
+public class TimezonesDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(ZoneId.getAvailableZoneIds());
+
+        ZoneId zone1 = zoneIdOf("Europe/Berlin");
+        System.out.println(zone1.getRules());
+
+        ZoneId zone2 = zoneIdOf("Brazil/East");
+        System.out.println(zone2.getRules());
+    }
+
+    public static ZoneId zoneIdOf(String string) {
+
+        return ZoneId.of(string);
+    }
+}
