d92bfe335c68703a664c89c6e55705f6872f15d4
bean <18222997200@163.com>
2018-09-13 10:19:27 +0800

    Function 接口有一个参数并且返回一个结果，并附带了一些可以和其他函数组合的默认方法（compose, andThen）

---
+++ src/org/happybean/sam/FunctionDemo.java
@@ -0,0 +1,20 @@
+package org.happybean.sam;
+
+import java.util.function.Function;
+
+/**
+ * @author wgt
+ * @date 2018-09-13
+ * @description Function:接口有一个参数并且返回一个结果，并附带了一些可以和其他函数组合的默认方法（compose, andThen）
+ **/
+public class FunctionDemo {
+
+    public static void main(String[] args) {
+
+        Function<String, Integer> toInteger = Integer::valueOf;
+        toInteger.apply("2018");
+
+        Function<String, String> backToString = toInteger.andThen(String::valueOf);
+        backToString.apply("2018");
+    }
+}
